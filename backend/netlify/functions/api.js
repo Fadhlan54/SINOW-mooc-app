16bb1ee390db0bea838c3524c5a5463ec061686b
Fadhlan54 <muhammadfadhlan0011@gmail.com>
2025-12-18 17:15:12 +0700

    fix deployment bug

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -1,11 +1,5 @@
 const serverless = require('serverless-http')
 
-const { PORT = 3000, BASE_URL } = process.env
-
 const app = require('../../index')
-
-app.listen(PORT, () => {
-  console.log(`Server berjalan di: ${BASE_URL}`)
-})
 
 module.exports.handler = serverless(app)
